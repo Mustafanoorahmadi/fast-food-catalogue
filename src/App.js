a2fbf6ca8e31edb01885d420b8f5fe413ce016ad
Mostafa <noorahmadimustafa204@gmail.com>
2024-08-18 12:22:37 +0430

    app components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import CategoryList from "./CategoryList/categoryList";
+import Header from "./Header/header";
+import Loading from "./Loading/loading";
+import FastFoodList from "./FastFoodList/fastFoodList";
+import SearchBar from "./SearchBar/searchBar";
+import notFound from "./assets/images/404.png";
+import useAxios from "./useAxios";
 
 function App() {
+  const [url, setUrl] = useState("/FastFood/list");
+  const [fastFoodItems, , loading] = useAxios({
+    url,
+  });
+
+  const filterItems = (categoryId) => {
+    setUrl(`/FastFood/list/${categoryId ? "?categoryId=" + categoryId : ""}`);
+  };
+
+  // search input
+  const searchItems = async (term) => {
+    setUrl(`/FastFood/search/${term ? "?term=" + term : ""}`);
+  };
+
+  const renderContent = () => {
+    if (loading) {
+      return <Loading theme="dark" />;
+    }
+    if (fastFoodItems.length === 0) {
+      return (
+        <>
+          <div className="alert alert-warning text-center ">
+            برای کلید واژه فوق هیچ آیتمی یافت نشد
+          </div>
+          <img className="mx-auto mt-5 d-block fade-in-horiz" src={notFound} />
+        </>
+      );
+    }
+    return <FastFoodList fastFoodItems={fastFoodItems} />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper bg-faded-dark">
+      <Header />
+      <CategoryList filterItems={filterItems}>
+        <SearchBar searchItems={searchItems} />
+      </CategoryList>
+      <div className="container mt-4">{renderContent()}</div>
     </div>
   );
 }
